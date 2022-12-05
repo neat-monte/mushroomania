6153842f906c415d99754bc06d4d4611716491aa
AaronvDiepen <AaronvDiepen@gmail.com>
2022-12-06 00:07:15 +0100

    Add SeasonTable Component including almost all of its functionality

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,8 +1,10 @@
 <template>
   <ScatterPlot :data="data" />
+  <SeasonTable :data="data" :style="{width: '50vw', height: '50vh'}"/>
 </template>
 
 <script setup>
+import SeasonTable from "../components/SeasonTable.vue";
 import ScatterPlot from "../components/ScatterPlot.vue";
 import data from "../data/mushroom-data.json";
 </script>
