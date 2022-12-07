d0cd9e588ec4259b4c53286c64338f63e5a6a432
AaronvDiepen <AaronvDiepen@gmail.com>
2022-12-07 16:30:49 +0100

    Change the x label to be more identifiable

--- src/utils/controls.js
+++ src/utils/controls.js
@@ -10,9 +10,9 @@ export const numericalProperties = [
 export const categoricalProperties = [
   {displayName: "Ediblity", propName: "poisonous", values: 
     {true: "Edible", false: "Non-edible"}},
-  {displayName: "Damage", propName: "doesBruiseOrBleed", values: 
-    {true: "Visbile", false: "Invisible"}},
-  {displayName: "Ring", propName: "hasRing", values: 
+  {displayName: "Damage Visibility", propName: "doesBruiseOrBleed", values: 
+    {true: "Bruising or Bleeding", false: "No changes"}},
+  {displayName: "Ring or not", propName: "hasRing", values: 
     {true: "One or more", false: "No ring"}},
   {displayName: "Habitat", propName: "habitat", values: 
     {g:"Grasses",l:"Leaves",m:"Meadows",p:"Paths",h:"Heaths",u:"Urban"
